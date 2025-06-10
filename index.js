ad09e1bda2d9e5829ea106f9a7768928ddba61e6
Gajendra Sharma <gajendrasharma0145@gmail.com>
2025-06-10 17:37:13 +0530

    changes in main file

--- index.js
+++ index.js
@@ -11,32 +11,30 @@ dotenv.config({});
 
 const app = express();
 
-connectDB()
+connectDB();
 
 // middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 const corsOptions = {
-    origin: process.env.FRONTEND_URL || 'http://localhost:5173',
-    credentials: true,
-}
+  origin:
+    process.env.FRONTEND_URL || "https://job-finder-seven-beta.vercel.app",
+  credentials: true,
+};
 app.use(cors(corsOptions));
 
-app.get("/",(req,res)=>{
-    res.send("Welcome to the server");
-})
+app.get("/", (req, res) => {
+  res.send("Welcome to the server");
+});
 
 const PORT = process.env.PORT || 3000;
-
 
 app.use("/api/v1/user", userRouter);
 app.use("/api/v1/company", companyRouter);
 app.use("/api/v1/job", jobRouter);
 app.use("/api/v1/application", applicantionRouter);
 
-
 app.listen(PORT, () => {
-    
-    console.log(`Server running at port ${PORT}`);
-})
+  console.log(`Server running at port ${PORT}`);
+});
